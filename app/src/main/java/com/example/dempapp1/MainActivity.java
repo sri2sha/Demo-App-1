f60569c0840407a6bd3316680ca1fb97dfd53dd8
sri2sha <sridhar.k@thundersoft.com>
2022-12-19 16:56:59 +0530

    First

---
+++ app/src/main/java/com/example/dempapp1/MainActivity.java
@@ -0,0 +1,33 @@
+package com.example.dempapp1;
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.Toast;
+import com.google.android.material.button.MaterialButton;
+import com.google.android.material.textfield.TextInputEditText;
+
+import java.util.Objects;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Objects.requireNonNull(getSupportActionBar()).hide();
+        configUserName();
+    }
+
+    public void configUserName(){
+        MaterialButton btnSave = (MaterialButton) findViewById(R.id.btnSave);
+        btnSave.setOnClickListener(v -> Toast.makeText(MainActivity.this, "", Toast.LENGTH_SHORT).show());
+    }
+
+    CharSequence getDetails(){
+        TextInputEditText etUserName = (TextInputEditText) findViewById(R.id.etUserName);
+        TextInputEditText etEmployeeId= (TextInputEditText) findViewById(R.id.etEmployeeId);
+
+        Toast.makeText(this, etUserName.getText(), Toast.LENGTH_SHORT).show();
+        Toast.makeText(this, etEmployeeId.getText(), Toast.LENGTH_SHORT).show();
+        return null;
+    }
+}
